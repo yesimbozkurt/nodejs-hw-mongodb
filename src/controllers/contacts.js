20f3ccb5ac34c3d08284fa8a8b739aca22dea835
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-12-09 13:38:34 +0300

    feat: implement error handling middleware and controller wrapper, update contact retrieval logic

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,17 +4,15 @@ import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getAllContactsController = async (req, res, next) => {
     try {
-
+        const contacts = await getAllContacts();
+        res.json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
     } catch (error) {
-
+        next(error);
     }
-    const contacts = await getAllContacts();
-
-    res.json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-    });
 };
 
 export const getContactByIdController = async (req, res, next) => {
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+//errorHandler
+export const errorHandler = (err, req, res, next) => {
+    res.status(500).json({
+        message: 'Something went wrong',
+        error: err.message,
+    });
+};
