43e3476e7656cfb7785a8a6b368bf04ac2b3a64f
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-12-19 00:01:57 +0300

    feat: improve getContactByIdController to handle missing contacts with 404 error

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -16,14 +16,15 @@ export const getAllContactsController = async (req, res, next) => {
 };
 
 export const getContactByIdController = async (req, res, next) => {
+    const { contactId } = req.params;
     try {
-        const { contactId } = req.params;
+
         const contact = await getContactById(contactId);
         if (!contact) {
 
             next(createHttpError(404, 'Contact not found'));
         }
-        res.json({
+        res.status(200).json({
             status: 200,
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
